795336e6e487e3d8a78ab8e09cf214a4a123de36
Durgesh Kumar <durgeshkumar7b@gmail.com>
2023-09-11 22:54:51 +0530

    Update fibonacci series.py

--- fibonacci series.py
+++ fibonacci series.py
@@ -8,3 +8,12 @@ while t <=z:
     x=y
     y=z
     z=x+y
+
+'''Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. 
+So, the first few numbers in the Fibonacci series look like this:
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+Each number in the series (starting from the third number) is the sum of the two numbers immediately before it.
+ For example, 2 is the sum of 1 and 1, 3 is the sum of 1 and 2, 5 is the sum of 2 and 3, and so on. 
+ This pattern continues indefinitely, creating a unique sequence of numbers with many interesting mathematical properties.'''
